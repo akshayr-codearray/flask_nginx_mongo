1903be10882a27c4c293f0129c8cda028f7597b4
akshayrathod911 <akshay.r@codearray.tech>
2023-01-31 18:39:30 +0530

    2nd commit for jenkins

--- flask/app.py
+++ flask/app.py
@@ -94,7 +94,7 @@ class One_User(Resource):
 
 class Default(Resource):
     def get(self):
-        return jsonify({"message":"Default endpoint"})
+        return jsonify({"message":"Default endpoint modified"})
 
 
 api.add_resource(User, '/api/user')
