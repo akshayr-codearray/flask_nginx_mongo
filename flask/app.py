bdc369e9baa1a53b5b07f261c9454c7206cf18fa
akshayrathod911 <akshay.r@codearray.tech>
2023-02-17 16:53:56 +0530

    updated code

--- flask/app.py
+++ flask/app.py
@@ -15,8 +15,9 @@ class User(Resource):
         user_list = []
         user_data = mongo.db.users
         for user in user_data.find():
-            user_list.append(user)
-        return dumps(user_list)
+            user_dict = {"id":user['user_id'], "name":user['name'], "email":user['email']}
+            user_list.append(user_dict)
+        return make_response(jsonify(user_list))
 
     def post(self):
         if request.is_json:
@@ -30,7 +31,7 @@ class User(Resource):
                     mongo.db.users.insert_one({"name": name, "email": email, "user_id": user_id})
                     return make_response({"message": "user added successfully"}, 201)
                 else:
-                    return make_response({"message":f"user with id {user_id} already exist please another id"},400)
+                    return make_response({"message":f"user with id {user_id} already exist please use another id"},400)
 
                 
             except KeyError:
@@ -40,33 +41,19 @@ class User(Resource):
                                                        "user_id": "id"}}, 400)
 
 
-class One_User(Resource):
-
-    def get(self):
-        if request.is_json:
-            try:
-                user = request.get_json()
-                u_id = user['user_id']
-                if u_id in [i['user_id'] for i in mongo.db.users.find()]:
-                    user = mongo.db.users.find_one({"user_id": u_id})
-                    return dumps(user)
-                else:
-                    return make_response({"message": "Enter valid 'user_id' "}, 404)
-
-            except KeyError:
-                return make_response({"message": "invalid json format", "valid_format": {"user_id": "id"}}, 400)
-
     def put(self):
         if request.is_json:
             try:
                 data = request.get_json()
+                if data['user_id'] not in [i['user_id'] for i in mongo.db.users.find()]:
+                    return make_response(jsonify({"message":"please enter valid 'user_id'"}))
+                else:
+                    myquery = {"user_id": data['user_id']}
+                    values = {"$set": {"email": data['email'], "name": data['name']}}
 
-                myquery = {"user_id": data['user_id']}
-                values = {"$set": {"email": data['email'], "name": data['name']}}
+                    mongo.db.users.update_one(myquery, values)
 
-                mongo.db.users.update_one(myquery, values)
-
-                return make_response({"message": "user updated successfully"}, 201)
+                    return make_response({"message": "user updated successfully"}, 201)
 
             except KeyError:
                 return make_response({"message": "invalid json format", "valid_format": {"user_id": "id",
@@ -74,22 +61,33 @@ class One_User(Resource):
                                                                                          "email": "your_email"
                                                                                          }}, 400)
 
-    def delete(self):
-        if request.is_json:
+
+class One_User(Resource):
+
+    def get(self,id):
             try:
-                data = request.get_json()
-                user_id = data['user_id']
-                # user_ids = []
-                # for user in mongo.db.users.find():
-                #     user_ids.append(user['user_id'])
-                if user_id in [i['user_id'] for i in mongo.db.users.find()]:
-                    mongo.db.users.delete_one({"user_id": user_id})
-                    return make_response({"message": f'user with id {user_id} deleted !!!'}, 200)
+                if id in [i['user_id'] for i in mongo.db.users.find()]:
+                    user = mongo.db.users.find_one({"user_id": id})
+                    one_user = [{"id":user['user_id'], "name":user['name'], "email":user['email']}]
+                    return make_response(jsonify(one_user))
                 else:
-                    return make_response({"message": "enter valid 'user_id'"}, 404)
+                    return make_response({"message": f"user with id {id} do not exist"}, 404)
 
             except KeyError:
-                return make_response({"message": "invalid json format", "valid_format": {"user_id": "id"}}, 400)
+                return make_response({"message": "something went"}, 400)
+
+    
+
+    def delete(self,id):
+            try:
+                if id in [i['user_id'] for i in mongo.db.users.find()]:
+                    mongo.db.users.delete_one({"user_id": id})
+                    return make_response({"message": f'user with id {id} deleted !!!'}, 200)
+                else:
+                    return make_response({"message": f"user with id {id} do not exist"}, 404)
+
+            except KeyError:
+                return make_response({"message": "something went wrong"}, 400)
 
 
 class Default(Resource):
@@ -98,5 +96,5 @@ class Default(Resource):
 
 
 api.add_resource(User, '/api/user')
-api.add_resource(One_User, '/api/one')
+api.add_resource(One_User, '/api/one/<int:id>')
 api.add_resource(Default,'/')
--- image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd flask
-minikube image load helloworld
